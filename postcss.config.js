acbc72c5f9d3b742d2d2bee4705fa758dde21aae
Keith <keithwhisper@gmail.com>
2021-03-17 15:11:13 +0800

    feat: enable tailwindcss

---
+++ postcss.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  plugins: {
+    'postcss-import': {},
+    tailwindcss: {},
+    'postcss-preset-env': {
+      stage: 3,
+      autoprefixer: { flexbox: 'no-2009' },
+      feature: { 'custom-properties': false },
+    },
+  },
+}
